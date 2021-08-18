a0e72c4a25a64971ca32dbfed043f484d22f7d41
Stefan923 <stefanpopescu923@gmail.com>
2021-08-18 23:46:25 +0300

    Made ProductRepository cross-origin

--- backend/src/main/java/me/stefan923/ecommerce/repository/ProductRepository.java
+++ backend/src/main/java/me/stefan923/ecommerce/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package me.stefan923.ecommerce.repository;
 
 import me.stefan923.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
