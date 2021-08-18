ff4f9d1f0bafd264ac983863119144208b67d789
Stefan923 <stefanpopescu923@gmail.com>
2021-08-18 22:14:26 +0300

    Added Product entity

---
+++ backend/src/main/java/me/stefan923/ecommerce/entity/Product.java
@@ -0,0 +1,50 @@
+package me.stefan923.ecommerce.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name="product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "sku")
+    private String sku;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @Column(name = "units_in_stock")
+    private int unitsInStock;
+
+    @CreationTimestamp
+    @Column(name = "date_created")
+    private LocalDate dateCreated;
+
+    @UpdateTimestamp
+    @Column(name = "last_updated")
+    private LocalDate lastUpdated;
+
+}
