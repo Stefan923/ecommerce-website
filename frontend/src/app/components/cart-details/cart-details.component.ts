a4c7883b18f584926d0a7c87b2db608851a48350
Stefan923 <stefanpopescu923@gmail.com>
2021-08-25 23:48:36 +0300

    Set up cart-status's TypeScript component

--- frontend/src/app/components/cart-details/cart-details.component.ts
+++ frontend/src/app/components/cart-details/cart-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CartItem } from 'src/app/common/cart-item';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart-details',
@@ -7,12 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartDetailsComponent implements OnInit {
 
+  cartItems: CartItem[] = [];
+
   totalPrice: number = 0.00;
   totalQuantity: number = 0;
 
-  constructor() { }
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
+    this.listCartDetails();
+  }
+
+  listCartDetails() {
+    this.cartItems = this.cartService.cartItems;
+
+    this.updateCartStatus();
+    this.cartService.computeCartTotals();
+  }
+
+  updateCartStatus() {
+    this.cartService.totalPrice.subscribe(
+      data => this.totalPrice = data
+    );
+
+    this.cartService.totalQuantity.subscribe(
+      data => this.totalQuantity = data
+    );
   }
 
 }
