e8f997aa10747fc92dfd15c0887180478e0bcf94
Stefan923 <stefanpopescu923@gmail.com>
2021-08-29 14:11:45 +0300

    Added a new entity: state

--- backend/src/main/java/me/stefan923/ecommerce/entity/State.java
+++ backend/src/main/java/me/stefan923/ecommerce/entity/State.java
@@ -3,25 +3,22 @@ package me.stefan923.ecommerce.entity;
 import lombok.Data;
 
 import javax.persistence.*;
-import java.util.Set;
 
 @Data
 @Entity
-@Table(name = "country")
-public class Country {
+@Table(name = "state")
+public class State {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private long id;
 
-    @Column(name = "code")
-    private String code;
-
     @Column(name = "name")
     private String name;
 
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "country")
-    private Set<State> states;
+    @ManyToOne
+    @JoinColumn(name = "country_id", nullable = false)
+    private Country country;
 
 }
