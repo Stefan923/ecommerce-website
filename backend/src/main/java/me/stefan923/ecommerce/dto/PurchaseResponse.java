f8ee727742733a1e247fcc7118572815834064cf
Stefan923 <stefanpopescu923@gmail.com>
2021-09-11 19:25:45 +0300

    Added a constructor with all arguments

--- backend/src/main/java/me/stefan923/ecommerce/dto/PurchaseResponse.java
+++ backend/src/main/java/me/stefan923/ecommerce/dto/PurchaseResponse.java
@@ -1,8 +1,10 @@
 package me.stefan923.ecommerce.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class PurchaseResponse {
 
     private String orderTrackingNumber;
