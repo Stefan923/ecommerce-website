50a365e82e3883ba93e3dc5a17da5a540f58c267
Stefan923 <stefanpopescu923@gmail.com>
2021-09-11 18:25:35 +0300

    Added a new entity: Customer

---
+++ backend/src/main/java/me/stefan923/ecommerce/entity/Customer.java
@@ -0,0 +1,28 @@
+package me.stefan923.ecommerce.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+}
