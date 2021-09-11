d4f1d33253f927bbcfc2d5e35953d7f983d02f31
Stefan923 <stefanpopescu923@gmail.com>
2021-09-11 18:44:56 +0300

    Added a new entity: OrderItem

---
+++ backend/src/main/java/me/stefan923/ecommerce/entity/OrderItem.java
@@ -0,0 +1,32 @@
+package me.stefan923.ecommerce.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "order_item")
+public class OrderItem {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    @Column(name = "product_id")
+    private long productId;
+
+}
