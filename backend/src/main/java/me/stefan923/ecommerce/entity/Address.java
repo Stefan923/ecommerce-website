d51b3bce6357583bf5ccba5084a5e7b6200705ce
Stefan923 <stefanpopescu923@gmail.com>
2021-09-11 18:28:14 +0300

    Added a new entity: Address

---
+++ backend/src/main/java/me/stefan923/ecommerce/entity/Address.java
@@ -0,0 +1,34 @@
+package me.stefan923.ecommerce.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "address")
+public class Address {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "state")
+    private String state;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "zip_code")
+    private String zipCode;
+
+}
